e21086e8bdf4322c1897c5af7de8709b7e6e9605
Thomas VERHOKEN <tverhoken@norauto.com>
2018-10-06 11:16:11 +0200

    Refactor form management

--- scripts/cat-form.js
+++ scripts/cat-form.js
@@ -1,10 +1,21 @@
-import { createCard, getCard, updateCard, deleteCard } from './data-service.js';
+import { getCard, createCard, updateCard, deleteCard } from './data-service.js';
 import { setElementContent, getElement, addClass, removeClass } from './dom-helper.js';
 
 (() => {
+  function getQueryParams() {
+    return location.search.substring(1).split('&').map(p => p.split('=')).reduce((acc, v) => { acc[v[0]] = v[1]; return acc; }, {});
+  }
+  function fillForm(card) {
+    getElement('#cardId').value = card.id;
+    getElement('#cardTitle').value = card.title;
+    getElement('#cardImage').value = card.imageUrl;
+    getElement('#cardDescription').value = card.description;
+  }
+
   function redirectToHomePage() {
     location.href = '/';
   }
+
   function prepareErrorHandling(hideForm) {
     return function handleError(error) {
       const errorContainer = getElement('#formError');
@@ -16,23 +27,30 @@ import { setElementContent, getElement, addClass, removeClass } from './dom-help
     }
   }
 
-  getElement('#cardForm').addEventListener('submit', e => {
+  function newCard() {
+    const title = getElement('#cardTitle').value;
+    const imageUrl = getElement('#cardImage').value;
+    const description = getElement('#cardDescription').value;
+    return {
+      title,
+      imageUrl,
+      description
+    };
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
     const form = e.target;
-    
+    const errorContainer = getElement('#formError');
+    if (errorContainer.classList.contains('d-none')) {
+      removeClass(errorContainer, 'd-none');
+    }
     removeClass(form, 'was-validated');
     if (!form.checkValidity()) {
       addClass(form, 'was-validated');
     } else {
-      const title = getElement('#cardTitle').value;
-      const imageUrl = getElement('#cardImage').value;
-      const description = getElement('#cardDescription').value;
-      const card = {
-        title,
-        imageUrl,
-        description
-      };
-      if (queryParams.cardId) {
+      const card = newCard();
+      if (getQueryParams().cardId) {
         card.id = parseInt(getElement('#cardId').value, 10);
         updateCard(card)
           .then(redirectToHomePage)
@@ -41,29 +59,27 @@ import { setElementContent, getElement, addClass, removeClass } from './dom-help
         createCard(card).then(redirectToHomePage);
       }
     }
-  });
+  }
+  
+  getElement('#cardForm').addEventListener('submit', handleSubmit);
+
   const deleteButton = getElement('.btn-danger');
 
   deleteButton.addEventListener('click', e => {
     e.preventDefault();
-    deleteCard(queryParams.cardId)
+    deleteCard(getQueryParams().cardId)
       .then(redirectToHomePage)
       .catch(prepareErrorHandling(false));
   });
 
   addClass(deleteButton, 'd-none');
 
-  const queryParams = location.search.substring(1).split('&').map(p => p.split('=')).reduce((acc, v) => { acc[v[0]] = v[1]; return acc; }, {});
-
-  if (queryParams.cardId) {
+  if (getQueryParams().cardId) {
     removeClass(deleteButton, 'd-none');
-    getCard(queryParams.cardId).then(card => {
-      getElement('#cardId').value = card.id;
-      getElement('#cardTitle').value = card.title;
-      getElement('#cardImage').value = card.imageUrl;
-      getElement('#cardDescription').value = card.description;
-    }).catch(prepareErrorHandling(true));
-  }
 
+    getCard(getQueryParams().cardId)
+      .then(fillForm)
+      .catch(prepareErrorHandling(true));
+  }
   setElementContent('#currentYear', new Date().getFullYear());
 })()
